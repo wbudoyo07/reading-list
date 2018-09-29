32a5d9a33788d2a93e04c9b1114fd13e3a969cb0
William Budoyo <williambu07@gmail.com>
2018-09-29 12:52:58 -0400

    test

---
+++ Unsolved/client/src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Books from "./pages/Books";
+import Detail from "./pages/Detail";
+import NoMatch from "./pages/NoMatch";
+import Nav from "./components/Nav";
+import {BrowserRouter as Router, Route, Switch } from "react-router-dom";
+const App = () => (
+  <Router>
+    <div>
+    <Nav />
+    <Switch>
+    <Route exact path ="/" component ={Books}/>
+    <Route exact path = "/books" component = {Books} />
+    <Route component = {NoMatch}/>
+    <Route
+        path="/books/:id"
+        component={Detail}
+      />
+    </Switch>
+
+    <Books />
+  </div>
+  </Router>
+
+);
+
+export default App;
